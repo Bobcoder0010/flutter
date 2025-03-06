ff858be303ad7c0837a9260cab7d16a826ca5440
Aashish pokhrel <dellizulter@gmail.com>
2025-03-06 08:30:55 +0545

    day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,102 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+
+//       // home: Scaffold(body:Container(
+//       //   //color:Colors.red,
+//       //   child:Column(
+//       //     mainAxisAlignment: MainAxisAlignment.end,
+//       //     crossAxisAlignment: CrossAxisAlignment.stretch,
+//       //     children: [
+//       //      // Text( "hello"),
+//       //      Container(color: Colors.red,height:100,width:100,),
+//       //      Container(color: Colors.blue,height:100,width:100,),
+
+//       //     ],
+//       //   ),
+//       home: Scaffold(
+//         body: Row(
+//           children: [
+//             Container(color: Colors.red, height: 100, width: 100),
+//             Container(
+//               color: Colors.blue,
+//               height: 100,
+//               width: 100,
+//               child: Column(
+//                 children: [Text("Name"), Text("Description"), Text("Price")],
+//               ),
+//             ),
+
+//             //Container(color: Colors.green,height:100,width:100,),
+//           ],
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
+  runApp(MyApp());
+}
 
-      // home: Scaffold(body:Container(
-      //   //color:Colors.red,
-      //   child:Column(
-      //     mainAxisAlignment: MainAxisAlignment.end,
-      //     crossAxisAlignment: CrossAxisAlignment.stretch,
-      //     children: [
-      //      // Text( "hello"),
-      //      Container(color: Colors.red,height:100,width:100,),
-      //      Container(color: Colors.blue,height:100,width:100,),
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-      //     ],
-      //   ),
-      home: Scaffold(
-        body: Row(
-          children: [
-            Container(color: Colors.red, height: 100, width: 100),
-            Container(
-              color: Colors.blue,
-              height: 100,
-              width: 100,
-              child: Column(
-                children: [Text("Name"), Text("Description"), Text("Price")],
-              ),
-            ),
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
 
-            //Container(color: Colors.green,height:100,width:100,),
-          ],
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int counter= 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(centerTitle: true, title: Text("day4")),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            "You have passed $counter times",
+            style: TextStyle(fontSize: 20),
+          ),
         ),
       ),
-    ),
-  );
+      floatingActionButton: Row(mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(onPressed: (){
+          setState(() {
+            if (counter>0){
+            counter--;
+            }
+          });
+        
+        }),
+    
+      SizedBox(width: 15),
+      FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+      ],
+      ),
+    );
+  }
 }
